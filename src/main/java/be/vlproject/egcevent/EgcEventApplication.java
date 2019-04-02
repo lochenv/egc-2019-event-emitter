a764f84739de9524b5767ff743fa684002afc6ee
Vincent Lochen <Vincent.Lochen@nbb.be>
2019-04-02 14:16:18 +0200

    first commit

---
+++ src/main/java/be/vlproject/egcevent/EgcEventApplication.java
@@ -0,0 +1,13 @@
+package be.vlproject.egcevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgcEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgcEventApplication.class, args);
+	}
+
+}
